47158cf7b37319fd654e1a909d1fad6d67483ec9
Lucas Salvador <lucas.salvador11358@gmail.com>
2019-12-08 12:13:49 -0300

    adicionando forms para geração de dados

--- client/src/App.vue
+++ client/src/App.vue
@@ -5,56 +5,100 @@
       color="primary"
       dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <span class="mr-2 headline">CSS - Calcula Simple Sheet</span>
     </v-app-bar>
-
     <v-content>
-      <HelloWorld/>
+      <v-container fluid>
+        <v-row>
+          <v-col 
+            cols="12"
+            md="4"
+            lg="4"
+            xl="4"
+          >
+            <v-card color="primary">
+              <v-card-title>Gere os dados para os cálculos</v-card-title>
+              <v-card-text>
+                <v-row>
+                  <v-col cols="12">
+                    <v-text-field
+                      v-model="qtdNumeros"
+                      label="Quantidade de números"
+                      required
+                    />
+                  </v-col>
+                  <v-col cols="12">
+                    <v-range-slider
+                      v-model="range"
+                      :max="max"
+                      :min="min"
+                      hide-details
+                      class="align-center"
+                      color="black"
+                    >
+                      <template v-slot:prepend>
+                        <v-text-field
+                          v-model="range[0]"
+                          class="mt-0 pt-0"
+                          hide-details
+                          single-line
+                          type="number"
+                          style="width: 60px"
+                        ></v-text-field>
+                      </template>
+                      <template v-slot:append>
+                        <v-text-field
+                          v-model="range[1]"
+                          class="mt-0 pt-0"
+                          hide-details
+                          single-line
+                          type="number"
+                          style="width: 60px"
+                        ></v-text-field>
+                      </template>
+                    </v-range-slider>
+                  </v-col>
+                </v-row>
+              </v-card-text>
+            </v-card>
+          </v-col>
+          <v-col 
+            cols="12"
+            md="8"
+            lg="8"
+            xl="8"
+          ></v-col>
+        </v-row>
+      </v-container>
     </v-content>
+    <v-footer
+      color="primary"
+      app
+    >
+      <span class="white--text">&copy;
+        {{ (new Date()).getFullYear() }}
+        <strong>Fatec São José dos Campos</strong>
+      </span>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
   },
 
   data: () => ({
     //
+    qtdNumeros: 0,
+    min: -1000,
+    max: 1000,
+    slider: 10,
+    range: [-20, 70]
   }),
 };
 </script>
